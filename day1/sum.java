f15a301992fe529b84124465d304cc2838125ba5
ratnamtiwari11-prog <ratnamtiwari11@gmail.com>
2026-06-03 23:38:05 +0530

    ratnam

--- day1/sum.java
+++ day1/sum.java
@@ -1,6 +1,6 @@
 public class sum
 {
-    public static void main(String[] args)
+        public static void main(String[] args)
     {
         int s=1+2+3+4+5;
         System.out.println("sum of first five natural numbers are  "+s);
